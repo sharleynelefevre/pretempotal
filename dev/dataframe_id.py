d45858fed696e839e3c692f74fe114f3d0e83466
Sharleyne Lefevre <sharleynelefevre@gmail.com>
2019-02-22 11:34:55 +0100

    actu fichiers csv / py + ajout fichier csv id comprenant la ponctuation

--- dev/dataframe_id.py
+++ dev/dataframe_id.py
@@ -32,92 +32,91 @@ dfId['idTimex'] = []
 dfId['signal'] = []
 dfId['idSignal'] = []
 
+dfId['event-4'] = []
+dfId['event+4'] = []
+
+
 for foldername in os.listdir(path):
-        if os.path.isdir(path+foldername):
-            for filename in os.listdir(path+foldername):
-                if '.tml' in filename:
-                    file = codecs.open(path+foldername+'/'+filename, 'r', 'utf8') 
-                    # parser le xml
-                    soup = BeautifulSoup(file.read(), 'xml') 
-                    file.close()
-
-                iterSent  = 0 # iterateur pour les identifiants de phrases
-                iterWord  = 0 # iterateur pour les identifiants de mots
-                iterEvent = 0 # iterateur pour les identifiants d'events
-                iterTimex = 0 # iterateur pour les identifiants de timex
-                iterSignal = 0 # iterateur pour les identifiants de signaux
+    if os.path.isdir(path+foldername):
+       for filename in os.listdir(path+foldername):
+           if '.tml' in filename:
+               file = codecs.open(path+foldername+'/'+filename, 'r', 'utf8') 
+               # parser le xml
+               soup = BeautifulSoup(file.read(), 'xml') 
+               file.close()
+            
+           iterSent  = 0 # iterateur pour les identifiants de phrases
+           iterWord  = 0 # iterateur pour les identifiants de mots
+           iterEvent = 0 # iterateur pour les identifiants d'events
+           iterTimex = 0 # iterateur pour les identifiants de timex
+           iterSignal = 0 # iterateur pour les identifiants de signaux
+            
+           for textTag in soup.find_all('TEXT'):
+               # on retire les '\n' 
+               text = textTag.text.strip('\n') # PB : il y a encore des '\n' qui empechent le decoupage en phrases
+            
+            
+           eventsTexts = []
+           for eventTag in soup.find_all('EVENT'):
+           #                    print(eventTag)
+               eventsTexts.append(eventTag.text)
+            
+           timexsTexts = []
+           for timexTag in soup.find_all('TIMEX3'):
+               timexsTexts.append(timexTag.text)
+            
+           signauxTexts = []
+           for signalTag in soup.find_all('SIGNAL'):
+               signauxTexts.append(signalTag.text)
                 
-                for textTag in soup.find_all('TEXT'):
-                    # on retire les '\n' 
-                    text = textTag.text.strip('\n') # PB : il y a encore des '\n' qui empechent le decoupage en phrases
                 
-                
-                eventsTexts = []
-                for e in soup.find_all('EVENT'):
-                    eventsTexts.append(e.text)
-                
-                timexsTexts = []
-                for t in soup.find_all('TIMEX3'):
-                    timexsTexts.append(t.text)
-                
-                signauxTexts = []
-                for s in soup.find_all('SIGNAL'):
-                    signauxTexts.append(s.text)
+           for sent in tokenize.sent_tokenize(str(text)):  
+               iterSent += 1
+            
+               # on decoupe les phrases en tokens
+               for wordTokenized in nltk.word_tokenize(sent):
+                   iterWord += 1
+                    
+                   # dans tous les cas on ajoute le docid le token son id et l'id de phrase dans laquelle il est
+                   dfId['docId'].append(soup.find('DOCID').text)
+                   dfId['word'].append(wordTokenized)
+                   dfId['idWord'].append(iterWord)
+                   dfId['idSent'].append(iterSent)
                     
                     
-                for sent in tokenize.sent_tokenize(str(text)):
-                    iterSent += 1
-                    
-                    # gestion de la ponctuation : eviter que les elements de ponctuation soient comptes comme des mots
-                    transTable = str.maketrans(',()._`\'-', 8*' ')
-                    sent = sent.translate(transTable)
-                    
-                    # on decoupe les phrases en tokens
-                    for wordTokenized in nltk.word_tokenize(sent):
-                        iterWord += 1
-                        
-                        # dans tous les cas on ajoute le docid le token son id et l'id de phrase dans laquelle il est
-                        dfId['docId'].append(soup.find('DOCID').text)
-                        dfId['word'].append(wordTokenized)
-                        dfId['idWord'].append(iterWord)
-                        dfId['idSent'].append(iterSent)
-                        
-                        
-                        # test si le token est un event
-                        if wordTokenized in eventsTexts :
-                            # si il en est un on incremente le compteur de 1
-                            iterEvent += 1
-                            # et on ajoute le token event dans la colonne event
-                            dfId['event'].append(wordTokenized)
-                            # on ajoute aussi son numero d'identifiant
-                            dfId['idEvent'].append(iterEvent)
-                        else:
-                            # sinon on ajoute une chaine vide
-                            dfId['event'].append('')
-                            dfId['idEvent'].append('')                            
-                        
-                        
-                        # on fait le meme test pour les timexs et les signaux                        
-                        if wordTokenized in timexsTexts :
-                            iterTimex += 1
-                            dfId['timex'].append(wordTokenized)
-                            dfId['idTimex'].append(iterTimex)
-                        else:
-                            dfId['timex'].append('')
-                            dfId['idTimex'].append('')
-
-
-                        if wordTokenized in signauxTexts :
-                            iterSignal += 1
-                            dfId['signal'].append(wordTokenized)
-                            dfId['idSignal'].append(iterSignal)
-                        else:
-                            dfId['signal'].append('')
-                            dfId['idSignal'].append('')   
-          
-                # mise en dataframe du dictionnaire de listes
-                res = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in dfId.items()]))
-
-                # ecriture dans le csv
-                res.to_csv('dev/dataframe_id.csv', sep=';', encoding='utf-8') 
-                
+                   # test si le token est un event
+                   if wordTokenized in eventsTexts :
+                       # si il en est un on incremente le compteur de 1
+                       iterEvent += 1
+                       # et on ajoute le token event dans la colonne event
+                       dfId['event'].append(wordTokenized)
+                       # on ajoute aussi son numero d'identifiant
+                       dfId['idEvent'].append(iterEvent)  
+                   else:
+                       # sinon on ajoute une chaine vide
+                       dfId['event'].append('')
+                       dfId['idEvent'].append('')                               
+                       
+                   # on fait le meme test pour les timexs et les signaux                        
+                   if wordTokenized in timexsTexts :
+                       iterTimex += 1
+                       dfId['timex'].append(wordTokenized)
+                       dfId['idTimex'].append(iterTimex)
+                   else:
+                       dfId['timex'].append('')
+                       dfId['idTimex'].append('')
+           
+           
+                   if wordTokenized in signauxTexts :
+                       iterSignal += 1
+                       dfId['signal'].append(wordTokenized)
+                       dfId['idSignal'].append(iterSignal)
+                   else:
+                       dfId['signal'].append('')
+                       dfId['idSignal'].append('')   
+             
+           # mise en dataframe du dictionnaire de listes
+           res = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in dfId.items()]))
+           
+           # ecriture dans le csv
+           res.to_csv('dev/dataframe_id_ponctuation.csv', sep=';', encoding='utf-8') 
--- dev/extractFeatures.py
+++ dev/extractFeatures.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Wed Feb  6 13:22:40 2019
 
 @author: Sharleyne-Lefevre
-"""
+'''
+
 from __future__ import print_function
 import os
 import csv
@@ -12,125 +13,215 @@ from bs4 import BeautifulSoup
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import *
 from nltk.stem import *
+from nltk import tokenize
+import nltk
 
-def extractFeaturesFromTml():
-    # chemin du repertoire des fichiers
-    path = "C:/Users/Sharleyne-Lefevre/Desktop/stage_LIFO/python/TBAQ-cleaned/TimeBank/" 
-    
+
+def extractFeaturesFromTml():    
     documents = []
+    
     # ouverture de chaque fichier du repertoire 
-    for filename in os.listdir(path):
-        if ".tml" in filename:
-            file = codecs.open(path+filename, "r", "utf8") 
-            # parser le xml
-            soup = BeautifulSoup(file.read(), "lxml")
-            file.close()
+    path = "dev/TBAQ-cleaned/"
+    for foldername in os.listdir(path):
+        if os.path.isdir(path+foldername):
+            for filename in os.listdir(path+foldername):
+                if '.tml' in filename:
+                    file = codecs.open(path+foldername+'/'+filename, 'r', 'utf8') 
+                    # parser le xml
+                    soup = BeautifulSoup(file.read(), 'xml') 
+                    file.close()
+
+                docId = soup.find('DOCID').text
+                
+                document = {}
+                document['docId'] = docId
+                
+                # ----------------------------------------------
+                #                     EVENTS
+                # ----------------------------------------------
+                # pour recuperer le stem de chaque event
+                stemmer = PorterStemmer()
+                # pour recuperer le lemme de chaque event
+                lemmatizer = WordNetLemmatizer()   
+                
+                events = {}
+                # recuperation des events
+                for eventTag in soup.find_all('EVENT'):
+                    event = eventTag.text
+                    verb = {}    
+                    dictEvent = {}
+                    
+                    # recuperation des attributs des events + ajout dans dictEvent 
+                    dictEvent['eid'] = eventTag.get('eid') 
+                    dictEvent['class'] = eventTag.get('class')
+                    dictEvent['stem'] = eventTag.get('stem')
+                    dictEvent['stemNLTK'] = stemmer.stem(event)
+
+                    # ajout de event dans le dictionnaire verbe
+                    verb['libelle'] = event
+                    
+                    # ajout du dictEvent dans le dictionnaire verbe
+                    verb['event'] = dictEvent
+                    
+                    # ajout du dictionnaire verbe dans le dictionnaire events
+                    events[eventTag.get('eid')] = verb
+
+                # on met chaque event dans document, la cle de events est 'verbes'
+                document['verbes'] = events
+                
+                # ----------------------------------------------
+                #                  INSTANCES
+                # ----------------------------------------------
+                for makeinstance in soup.find_all('MAKEINSTANCE'):
+                    instance = {}
+                    
+                    # recuperation des attributs des makeinstances et ajout dans le dictionnaire instance
+                    instance['eventid'] = makeinstance.get('eventid')
+                    instance['eiid'] = makeinstance.get('eiid')
+                    instance['aspect'] = makeinstance.get('aspect')
+                    instance['tense'] = makeinstance.get('tense')
+                    instance['pos'] = makeinstance.get('pos')
+                    instance['polarity'] = makeinstance.get('polarity')
+                    instance['modality'] = makeinstance.get('modality') 
+                    instance['cardinality'] = makeinstance.get('cardinality')                             
         
-        docId = soup.find('docid').text
-        
-        document = {}
-        document["docId"] = docId
-          
-        # ----------------------------------------------
-        #                     EVENTS
-        # ----------------------------------------------
-        # pour recuperer le stem de chaque event
-        stemmer = PorterStemmer()
-        # pour recuperer le lemme de chaque event
-        lemmatizer = WordNetLemmatizer()   
-        
-        events = {}
-        # recuperation des events
-        for eventTag in soup.find_all('event'):
-            event = eventTag.text
-            verb = {}    
-            dictEvent = {}
-            # recuperation des attributs des events + ajout dans dictEvent 
-            dictEvent['eid'] = eventTag.get('eid') 
-            dictEvent['class'] = eventTag.get('class')
-            dictEvent['stem'] = eventTag.get('stem')
-            dictEvent['stemNLTK'] = stemmer.stem(event)
-            dictEvent['lemmeNLTK'] = lemmatizer.lemmatize(event)
-            # ajout de event dans le dictionnaire verbe
-            verb['libelle'] = event
-            # ajout du dictEvent dans le dictionnaire verbe
-            verb['event'] = dictEvent
-            # ajout du dictionnaire verbe dans le dictionnaire events
-            events[eventTag.get('eid')] = verb
-        
-        # on met chaque event dans document, la cle de events est "verbes"
-        document['verbes'] = events
-        
-        # ----------------------------------------------
-        #                  INSTANCES
-        # ----------------------------------------------
-        for makeinstance in soup.find_all('makeinstance'):
-            dictInstance = {}
-            # recuperation des attributs des makeinstances et ajout dans le dictionnaire dictInstance
-            dictInstance["eventid"] = makeinstance.get('eventid')
-            dictInstance["eiid"] = makeinstance.get('eiid')
-            dictInstance["aspect"] = makeinstance.get('aspect')
-            dictInstance["tense"] = makeinstance.get('tense')
-            dictInstance["pos"] = makeinstance.get('pos')
-            dictInstance["polarity"] = makeinstance.get('polarity')
-            dictInstance["modality"] = makeinstance.get('modality')
-            
-            # on met le dictionnaire dictInstance dans le chemin : document > eid > instance > dictInstance
-            # dictInstance a pour cle "instance"
-            document['verbes'][makeinstance.get('eventid')]['instance'] = dictInstance
-        
-        # ajout du document contenant les informations des events qu'il contient
-        documents.append(document)
+                    # on met le dictionnaire instance dans le chemin : document > eid > instance > dictionaire instance
+                    # instance a pour cle 'instance'
+                    document['verbes'][makeinstance.get('eventID')]['instance'] = instance
+                
+                for verb in document['verbes']:
+                    verb = document['verbes'][verb]
+                    event = verb['libelle']
+                    
+                    # on n'a pas toujours d'instance pour un event
+                    # si on en a un
+                    if 'instance' in verb.keys():  
+                        # on recupere le pos
+                        pos = verb['instance']['pos'][0].lower()
+                    else :
+#                        print(docId + " " + event) # /!\ Events sans instance 
+                        # sinon on assigne none au pos des events n'ayant pas d'instance
+                        pos = None
+                    
+                    eid = verb['event']['eid']
+                    if not pos in ['u', 'p', 'o', None]:  # NLTK ne prend pas en compte les p et les o (OTHER) et les u (UNKNOWN)
+                        document['verbes'][eid]['event']['lemmeNltk'] = lemmatizer.lemmatize(event, pos)
+                    else:
+                        document['verbes'][eid]['event']['lemmeNltk'] = 'unknown'
+                
+                # ----------------------------------------------
+                #                     TIMEXS
+                # ----------------------------------------------        
+                timexs = {}
+                for timexTag in soup.find_all('TIMEX3'):
+                    timex = timexTag.text
+                    
+                    word = {}
+                    dictTimex = {}
+                    
+                    dictTimex['tid'] = timexTag.get('tid')
+                    dictTimex['type'] = timexTag.get('type')
+                    dictTimex['value'] = timexTag.get('value') 
+                    dictTimex['mod'] = timexTag.get('mod') 
+                    dictTimex['temporalFunction'] = timexTag.get('temporalFunction') 
+                    dictTimex['functionInDocument'] = timexTag.get('functionInDocument') 
+                    dictTimex['beginPoint'] = timexTag.get('beginPoint') 
+                    dictTimex['endPoint'] = timexTag.get('endPoint') 
+                    dictTimex['quant'] = timexTag.get('quant') 
+                    dictTimex['freq'] = timexTag.get('freq') 
+                    dictTimex['anchorTimeID'] = timexTag.get('anchorTimeID') 
+                    
+                    word['libelle'] = timex
+                    word['timex'] = dictTimex
+                    timexs[timexTag.get('tid')] = word
+                
+                document['timexs'] = timexs
+                
+                # ----------------------------------------------
+                #                     SIGNAUX
+                # ----------------------------------------------        
+                signaux = {}
+                for signalTag in soup.find_all('SIGNAL'):
+                    signal = signalTag.text
+                    
+                    strSignal = {}
+                    dictSignal = {}
+                    
+                    dictSignal['sid'] = signalTag.get('sid')
+                    
+                    strSignal['libelle'] = signal
+                    strSignal['signal'] = dictTimex
+                    signaux[signalTag.get('sid')] = strSignal
+                
+                document['signaux'] = signaux
+                # ajout du document contenant les informations des events qu'il contient
+                documents.append(document)
+
     return documents
 
 
-def display(documents): # permet de mieux visualiser les données sur Python
-    for document in documents:
-        print("Document : " + str(document["docId"]))
-        for verb in document["verbes"]:
-            verb = document["verbes"][verb]
-            print("\t Libelle  : " + str(verb["libelle"]))
-            print("\t Event    : " + str(verb["event"]))
-            if "instance" in verb.keys():
-                print("\t Instance : " + str(verb["instance"]))
-            print()
-        print("\n\n")
-
-
-def writeCSV(documents):
-    # chemin du repertoire du fichier csv
-    path = "C:/Users/Sharleyne-Lefevre/Desktop/stage_LIFO/python/csv-features.csv" 
-    
+def writeCsvEvent(documents):    
     # ouverture du csv en ecriture
-    with open(path, 'w', newline='') as f:
+    with open('Dev/csv_features_events.csv', 'w', newline='') as f:
         # en-tete des colonnes
-        fieldnames = ["Libelle", "docID", "eventId", "eventInstanceId", "Class", "Stem", "StemNltk", "LemmeNltk", "Aspect", "Tense", "POS", "Polarity", "Modality"]
+        fieldnames = ['Libelle', 'docID', 'eid', 'eiid', 'Class', 'Stem', 'StemNltk', 'LemmeNltk', 'Aspect', 'Tense', 'POS', 'Polarity', 'Modality', 'Cardinality']
         # le writer est au format dictionnaire
         writer = csv.DictWriter(f, fieldnames = fieldnames, delimiter = ';')
 
         writer.writeheader()
         
         for document in documents:
-            for verb in document["verbes"]:
-                verb = document["verbes"][verb]
-                if "instance" in verb.keys():
+            for verb in document['verbes']:
+                
+                verb = document['verbes'][verb]
+                if 'instance' in verb.keys():
                     # remplissage des colonnes avec les elements provenant de event et instance + docId
-                    writer.writerow({"docID" : document["docId"], 
-                                     "Libelle" : verb["libelle"],
-                                     "eventId" : verb["event"]["eid"],
-                                     "Class" : verb["event"]["class"],
-                                     "Stem" : verb["event"]["stem"],
-                                     "StemNltk" : verb["event"]["stemNLTK"],
-                                     "LemmeNltk" : verb["event"]["lemmeNLTK"],
-                                     "eventInstanceId" : verb["instance"]["eiid"],
-                                     "Aspect" : verb["instance"]["aspect"],
-                                     "Tense" : verb["instance"]["tense"],
-                                     "POS" : verb["instance"]["pos"],
-                                     "Polarity" : verb["instance"]["polarity"],
-                                     "Modality" : verb["instance"]["modality"]})
+                    writer.writerow({'docID' : document['docId'], 
+                                     'Libelle' : verb['libelle'],
+                                     'eid' : verb['event']['eid'],
+                                     'Class' : verb['event']['class'],
+                                     'Stem' : verb['event']['stem'],
+                                     'StemNltk' : verb['event']['stemNLTK'],
+                                     'LemmeNltk' : verb['event']['lemmeNltk'], 
+                                     'eiid' : verb['instance']['eiid'],
+                                     'Aspect' : verb['instance']['aspect'],
+                                     'Tense' : verb['instance']['tense'],
+                                     'POS' : verb['instance']['pos'],
+                                     'Polarity' : verb['instance']['polarity'],
+                                     'Modality' : verb['instance']['modality'],
+                                     'Cardinality' : verb['instance']['cardinality']})
 
     f.close()
 
+
+def writeCsvTimex(documents):    
+    with open('Dev/csv_features_timex.csv', 'w', newline='') as f:
+        fieldnames = ['Libelle', 'docID', 'tid', 'Type', 'Value', 'Mod', 'temporalFunction', 'functionInDocument', 'beginPoint', 'endPoint', 'quant', 'freq', 'anchorTimeID']
+        writer = csv.DictWriter(f, fieldnames = fieldnames, delimiter = ';')
+
+        writer.writeheader()
+        
+        for document in documents:
+            for word in document['timexs']:
+                word = document['timexs'][word]
+                writer.writerow({'docID' : document['docId'], 
+                                 'Libelle' : word['libelle'],
+                                 'tid' : word['timex']['tid'],
+                                 'Type' : word['timex']['type'],
+                                 'Value' : word['timex']['value'],
+                                 'Mod' : word['timex']['mod'],
+                                 'temporalFunction' : word['timex']['temporalFunction'],
+                                 'functionInDocument' : word['timex']['functionInDocument'],
+                                 'beginPoint' : word['timex']['beginPoint'],
+                                 'endPoint' : word['timex']['endPoint'],
+                                 'quant' : word['timex']['quant'],
+                                 'freq' : word['timex']['freq'],
+                                 'anchorTimeID' : word['timex']['anchorTimeID']})
+
+    f.close()
+
+# En cours : csv signaux / ajout context     
+    
 documents = extractFeaturesFromTml()
-#display(documents) # a decommenter pour voir, dans chaque document, tous les events trouves
-writeCSV(documents)
+writeCsvEvent(documents)
+writeCsvTimex(documents)
