b943b5ab304d01fbec1158a56d8e7f63b6284cee
Sharleyne Lefevre <sharleynelefevre@gmail.com>
2019-03-07 10:43:53 +0100

    sep $ au lieu de - / input txt au lieu de tml dans dataframe_id.py / algo regroupement

--- dev/dataframe_id.py
+++ dev/dataframe_id.py
@@ -7,19 +7,13 @@ Created on Tue Feb 19 09:40:32 2019
 
 import os
 import nltk
-import codecs
 import pandas as pd
-from nltk import tokenize
-from bs4 import BeautifulSoup
-from pandas import DataFrame as df
-
-
-path = 'dev/TBAQ-cleaned/'
+from os import path
+import nltk
 
 dfId  = {}  
 
 dfId['word'] = []
-dfId['docId'] = []
 dfId['idWord'] = []
 dfId['idSent'] = []
 
@@ -32,91 +26,109 @@ dfId['idTimex'] = []
 dfId['signal'] = []
 dfId['idSignal'] = []
 
-dfId['event-4'] = []
-dfId['event+4'] = []
+
+def openFilesTxt():
+    path_tbaq = 'dev/TBAQ-txt/'
+    texts = []
+    for foldername in os.listdir(path_tbaq):
+        if os.path.isdir(path.join(path_tbaq, foldername)):
+           for filename in os.listdir(path.join(path_tbaq, foldername)):
+               (basename, ext) = path.splitext(filename)
+               if ext == '.txt':
+                   with open(path.join(path_tbaq, foldername, filename), 'r', encoding='utf8') as file:
+                       texts.append(file.read())
+    return texts
 
 
-for foldername in os.listdir(path):
-    if os.path.isdir(path+foldername):
-       for filename in os.listdir(path+foldername):
-           if '.tml' in filename:
-               file = codecs.open(path+foldername+'/'+filename, 'r', 'utf8') 
-               # parser le xml
-               soup = BeautifulSoup(file.read(), 'xml') 
-               file.close()
-            
-           iterSent  = 0 # iterateur pour les identifiants de phrases
-           iterWord  = 0 # iterateur pour les identifiants de mots
-           iterEvent = 0 # iterateur pour les identifiants d'events
-           iterTimex = 0 # iterateur pour les identifiants de timex
-           iterSignal = 0 # iterateur pour les identifiants de signaux
-            
-           for textTag in soup.find_all('TEXT'):
-               # on retire les '\n' 
-               text = textTag.text.strip('\n') # PB : il y a encore des '\n' qui empechent le decoupage en phrases
-            
-            
-           eventsTexts = []
-           for eventTag in soup.find_all('EVENT'):
-           #                    print(eventTag)
-               eventsTexts.append(eventTag.text)
-            
-           timexsTexts = []
-           for timexTag in soup.find_all('TIMEX3'):
-               timexsTexts.append(timexTag.text)
-            
-           signauxTexts = []
-           for signalTag in soup.find_all('SIGNAL'):
-               signauxTexts.append(signalTag.text)
-                
-                
-           for sent in tokenize.sent_tokenize(str(text)):  
-               iterSent += 1
-            
-               # on decoupe les phrases en tokens
-               for wordTokenized in nltk.word_tokenize(sent):
-                   iterWord += 1
-                    
-                   # dans tous les cas on ajoute le docid le token son id et l'id de phrase dans laquelle il est
-                   dfId['docId'].append(soup.find('DOCID').text)
-                   dfId['word'].append(wordTokenized)
-                   dfId['idWord'].append(iterWord)
-                   dfId['idSent'].append(iterSent)
-                    
-                    
-                   # test si le token est un event
-                   if wordTokenized in eventsTexts :
-                       # si il en est un on incremente le compteur de 1
-                       iterEvent += 1
-                       # et on ajoute le token event dans la colonne event
-                       dfId['event'].append(wordTokenized)
-                       # on ajoute aussi son numero d'identifiant
-                       dfId['idEvent'].append(iterEvent)  
-                   else:
-                       # sinon on ajoute une chaine vide
-                       dfId['event'].append('')
-                       dfId['idEvent'].append('')                               
-                       
-                   # on fait le meme test pour les timexs et les signaux                        
-                   if wordTokenized in timexsTexts :
-                       iterTimex += 1
-                       dfId['timex'].append(wordTokenized)
-                       dfId['idTimex'].append(iterTimex)
-                   else:
-                       dfId['timex'].append('')
-                       dfId['idTimex'].append('')
+def createId():
+   files = openFilesTxt()
+
+   eventsTexts = []
+   timexsTexts = []
+   signauxTexts = []
+   
+   for file in files:
+       iterSent  = 0 # iterateur pour les identifiants de phrases
+       iterWord  = 0 # iterateur pour les identifiants de mots
+       iterEvent = 0 # iterateur pour les identifiants d'events
+       iterTimex = 0 # iterateur pour les identifiants de timex
+       iterSignal = 0 # iterateur pour les identifiants de signaux
+       
+       for w in file.split():
+           if '#e' in w:
+               eventsTexts.append(w)
+               
+           if '#t' in w:
+               timexsTexts.append(w)
+              
+           if '#s' in w:
+               signauxTexts.append(w)
            
+       sents = nltk.sent_tokenize(file)
+       for sent in sents:
+           iterSent += 1
+           tokens = nltk.word_tokenize(sent)
+           new = []
+           sep = '$'
            
-                   if wordTokenized in signauxTexts :
-                       iterSignal += 1
-                       dfId['signal'].append(wordTokenized)
-                       dfId['idSignal'].append(iterSignal)
-                   else:
-                       dfId['signal'].append('')
-                       dfId['idSignal'].append('')   
-             
-           # mise en dataframe du dictionnaire de listes
-           res = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in dfId.items()]))
-           
-           # ecriture dans le csv
-           res.to_csv('dev/dataframe_id_ponctuation.csv', sep=';', encoding='utf-8') 
+           i = 0
+           while i < len(tokens):
+               t = tokens[i]
+               if t == '#':
+                   t2 = tokens[i-1] + t + tokens[i+1]
+                   new.append(t2)
+                   indexT2 = new.index(t2)
+                   new.remove(new[indexT2-1])
+                   i += 2
+               else:
+                   new.append(t)
+                   i += 1
+
+           while sep in new:
+               indexSep = new.index(sep)
+               new[indexSep-1] += new[indexSep] + new[indexSep+1]
+               new.remove(new[indexSep+1])
+               new.remove(new[indexSep])
+
+           for word in new:
+               iterWord += 1
+               dfId['word'].append(word)
+               dfId['idWord'].append(iterWord)
+               dfId['idSent'].append(iterSent)  
+               
+               if word in eventsTexts:
+                   iterEvent += 1
+                   # et on ajoute le token event dans la colonne event
+                   dfId['event'].append(word)
+                   # on ajoute aussi son numero d'identifiant
+                   dfId['idEvent'].append(iterEvent)  
+               else:
+                   # sinon on ajoute une chaine vide
+                   dfId['event'].append('')
+                   dfId['idEvent'].append('')   
+                   
+                   
+               if word in timexsTexts :
+                   iterTimex += 1
+                   dfId['timex'].append(word)
+                   dfId['idTimex'].append(iterTimex)
+               else:
+                   dfId['timex'].append('')
+                   dfId['idTimex'].append('')
+                   
+                   
+               if word in signauxTexts :
+                   iterSignal += 1
+                   dfId['signal'].append(word)
+                   dfId['idSignal'].append(iterSignal)
+               else:
+                   dfId['signal'].append('')
+                   dfId['idSignal'].append('') 
+
+
+       # mise en dataframe du dictionnaire de listes
+       res = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in dfId.items()]))
+       # ecriture dans le csv
+       res.to_csv('dev/CSV/dataframe_id_ponctuation.csv', sep=';', encoding='utf-8') 
+
+createId()
--- dev/transformation_xml_to_txt.py
+++ dev/transformation_xml_to_txt.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Tue Feb 26 13:21:07 2019
 
 @author: Sharleyne-Lefevre
-"""
+'''
 from bs4 import BeautifulSoup
-import codecs
 import os
-import re
 
 from os import path
 
-path_tbaq = "dev/TBAQ-cleaned/"
+path_tbaq = 'dev/TBAQ-cleaned/'
 for foldername in os.listdir(path_tbaq):
     if os.path.isdir(path.join(path_tbaq, foldername)):
-        output_dir = path.join("dev", "TBAQ_txt", foldername)
+        output_dir = path.join('dev', 'TBAQ-txt', foldername)
         if not path.isdir(output_dir):
-            os.mkdir(path.join("dev", "TBAQ_txt", foldername))
+            os.mkdir(path.join('dev', 'TBAQ-txt', foldername))
             
         for filename in os.listdir(path.join(path_tbaq, foldername)):
             (basename, ext) = path.splitext(filename)
@@ -27,21 +25,21 @@ for foldername in os.listdir(path_tbaq):
 
             for eventTag in soup.find_all('EVENT'):  
                 # mettre les events au format libelleE#eid
-                new_event = ' '.join(['{}#{}'.format(w, eventTag.get('eid')) for w in eventTag.text.split(' ')]) 
+                new_event = '$'.join(['{}#{}'.format(w, eventTag.get('eid')) for w in eventTag.text.split(' ')]) 
                 eventTag.string.replace_with(new_event)
                 
 
             for timexTag in soup.find_all('TIMEX3'):   
                 # mettre les timexs au format libelleT#tid
-                new_timex = ' '.join(['{}#{}'.format(w, timexTag.get('tid')) for w in timexTag.text.split(' ')]) 
+                new_timex = '$'.join(['{}#{}'.format(w, timexTag.get('tid')) for w in timexTag.text.split(' ')]) 
                 timexTag.string.replace_with(new_timex)
 
                 
             for signalTag in soup.find_all('SIGNAL'):
                 # mettre les signaux au format libelleS#sid
-                new_signal = ' '.join(['{}#{}'.format(w, signalTag.get('sid')) for w in signalTag.text.split(' ')]) 
+                new_signal = '$'.join(['{}#{}'.format(w, signalTag.get('sid')) for w in signalTag.text.split(' ')]) 
                 signalTag.string.replace_with(new_signal)
 
             txt = soup.text.rstrip('\n')
-            with open(path.join(output_dir, basename+".txt"), "w", encoding="utf8") as fileW:
+            with open(path.join(output_dir, basename+'.txt'), 'w', encoding='utf8') as fileW:
                 fileW.write(txt)
